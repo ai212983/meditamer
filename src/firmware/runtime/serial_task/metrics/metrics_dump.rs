2dca3127117e2e56797f56ef35c2beff535e93eb
Dimitri Fedorov <df.41356@gmail.com>
2026-03-12 18:31:17 +0100

    refactor(runtime): split serial telemetry metrics

--- src/firmware/runtime/serial_task/metrics/metrics_dump.rs
+++ src/firmware/runtime/serial_task/metrics/metrics_dump.rs
@@ -3,11 +3,10 @@ use core::{fmt::Write, sync::atomic::Ordering};
 use crate::firmware::{
     config::{LAST_MARBLE_REDRAW_MS, MAX_MARBLE_REDRAW_MS},
     telemetry,
-    touch::debug_log::uart_write_all,
     types::SerialUart,
 };
 
-use super::commands::{TelemetryDomain, TelemetrySetOperation};
+use super::{write_line, write_metrics_net_lines};
 
 pub(super) async fn write_metrics_lines(uart: &mut SerialUart) {
     let last_ms = LAST_MARBLE_REDRAW_MS.load(Ordering::Relaxed);
@@ -20,7 +19,7 @@ pub(super) async fn write_metrics_lines(uart: &mut SerialUart) {
         "METRICS MARBLE_REDRAW_MS={} MAX_MS={}\r\n",
         last_ms, max_ms
     );
-    let _ = uart_write_all(uart, line.as_bytes()).await;
+    write_line(uart, line).await;
 
     let mut wifi_line = heapless::String::<256>::new();
     let _ = write!(
@@ -34,7 +33,7 @@ pub(super) async fn write_metrics_lines(uart: &mut SerialUart) {
         snapshot.wifi_scan_empty,
         snapshot.wifi_scan_target_hits,
     );
-    let _ = uart_write_all(uart, wifi_line.as_bytes()).await;
+    write_line(uart, wifi_line).await;
 
     let mut wifi_link_line = heapless::String::<192>::new();
     let _ = write!(
@@ -46,7 +45,7 @@ pub(super) async fn write_metrics_lines(uart: &mut SerialUart) {
         snapshot.wifi_link_rssi_samples,
         snapshot.wifi_link_rssi_low_samples,
     );
-    let _ = uart_write_all(uart, wifi_link_line.as_bytes()).await;
+    write_line(uart, wifi_link_line).await;
 
     let mut wifi_reassoc_line = heapless::String::<512>::new();
     let _ = write!(
@@ -69,7 +68,7 @@ pub(super) async fn write_metrics_lines(uart: &mut SerialUart) {
         snapshot.wifi_reassoc_connect_ms_total,
         snapshot.wifi_reassoc_connect_ms_max,
     );
-    let _ = uart_write_all(uart, wifi_reassoc_line.as_bytes()).await;
+    write_line(uart, wifi_reassoc_line).await;
 
     let mut wifi_scan_diag_line = heapless::String::<512>::new();
     let _ = write!(
@@ -87,7 +86,7 @@ pub(super) async fn write_metrics_lines(uart: &mut SerialUart) {
         snapshot.wifi_reassoc_scan_passive_ms_max,
         snapshot.wifi_reassoc_last_scan_channel,
     );
-    let _ = uart_write_all(uart, wifi_scan_diag_line.as_bytes()).await;
+    write_line(uart, wifi_scan_diag_line).await;
 
     let mut wifi_reason_diag_line = heapless::String::<512>::new();
     let _ = write!(
@@ -109,7 +108,7 @@ pub(super) async fn write_metrics_lines(uart: &mut SerialUart) {
         snapshot.wifi_reassoc_last_probe_idx,
         snapshot.wifi_reassoc_last_stage,
     );
-    let _ = uart_write_all(uart, wifi_reason_diag_line.as_bytes()).await;
+    write_line(uart, wifi_reason_diag_line).await;
 
     let mut upload_line = heapless::String::<384>::new();
     let _ = write!(
@@ -130,7 +129,7 @@ pub(super) async fn write_metrics_lines(uart: &mut SerialUart) {
         snapshot.sd_upload_session_timeout_aborts,
         snapshot.sd_upload_session_mode_off_aborts,
     );
-    let _ = uart_write_all(uart, upload_line.as_bytes()).await;
+    write_line(uart, upload_line).await;
 
     let mut upload_phase_line = heapless::String::<320>::new();
     let _ = write!(
@@ -145,7 +144,7 @@ pub(super) async fn write_metrics_lines(uart: &mut SerialUart) {
         snapshot.upload_http_upload_request_ms_total,
         snapshot.upload_http_upload_request_ms_max,
     );
-    let _ = uart_write_all(uart, upload_phase_line.as_bytes()).await;
+    write_line(uart, upload_phase_line).await;
 
     let mut upload_decomp_line = heapless::String::<512>::new();
     let _ = write!(
@@ -165,7 +164,7 @@ pub(super) async fn write_metrics_lines(uart: &mut SerialUart) {
         snapshot.upload_http_upload_chunk_samples_total,
         snapshot.upload_http_upload_chunk_samples_dropped,
     );
-    let _ = uart_write_all(uart, upload_decomp_line.as_bytes()).await;
+    write_line(uart, upload_decomp_line).await;
 
     let mut upload_rtt_line = heapless::String::<512>::new();
     let _ = write!(
@@ -190,137 +189,7 @@ pub(super) async fn write_metrics_lines(uart: &mut SerialUart) {
         snapshot.sd_upload_rtt_remove_ms_total,
         snapshot.sd_upload_rtt_remove_ms_max,
     );
-    let _ = uart_write_all(uart, upload_rtt_line.as_bytes()).await;
+    write_line(uart, upload_rtt_line).await;
 
     write_metrics_net_lines(uart).await;
-}
-
-pub(super) async fn write_metrics_net_lines(uart: &mut SerialUart) {
-    let snapshot = telemetry::snapshot();
-    let ip = snapshot.upload_http_ipv4.unwrap_or([0, 0, 0, 0]);
-
-    let mut net_line = heapless::String::<160>::new();
-    let _ = write!(
-        &mut net_line,
-        "METRICS NET wifi_connected={} http_listening={} ip={}.{}.{}.{}\r\n",
-        if snapshot.wifi_link_connected { 1 } else { 0 },
-        if snapshot.upload_http_listening { 1 } else { 0 },
-        ip[0],
-        ip[1],
-        ip[2],
-        ip[3],
-    );
-    let _ = uart_write_all(uart, net_line.as_bytes()).await;
-
-    let mut liveness_line = heapless::String::<224>::new();
-    let _ = write!(
-        &mut liveness_line,
-        "METRICS LIVENESS accept_link_reset={} health={} wifi_watchdog_disc={}\r\n",
-        snapshot.upload_http_accept_link_resets,
-        snapshot.upload_http_health_requests,
-        snapshot.wifi_connected_watchdog_disconnects,
-    );
-    let _ = uart_write_all(uart, liveness_line.as_bytes()).await;
-
-    let mut boot_line = heapless::String::<96>::new();
-    let _ = write!(
-        &mut boot_line,
-        "METRICS BOOT reset_code={}\r\n",
-        snapshot.boot_reset_reason_code,
-    );
-    let _ = uart_write_all(uart, boot_line.as_bytes()).await;
-
-    let mut net_pipeline_line = heapless::String::<512>::new();
-    let _ = write!(
-        &mut net_pipeline_line,
-        "METRICS NET_PIPELINE dhcp_wait_n={} dhcp_wait_ms={} dhcp_wait_ms_max={} dhcp_ready={} gate_wifi_down={} gate_link_down={} gate_no_ipv4={} listener_on={} listener_off={} accept_wait_n={} accept_wait_ms={} accept_wait_ms_max={}\r\n",
-        snapshot.net_pipeline_dhcp_wait_count,
-        snapshot.net_pipeline_dhcp_wait_ms_total,
-        snapshot.net_pipeline_dhcp_wait_ms_max,
-        snapshot.net_pipeline_dhcp_ready_count,
-        snapshot.net_pipeline_gate_wifi_down,
-        snapshot.net_pipeline_gate_link_down,
-        snapshot.net_pipeline_gate_no_ipv4,
-        snapshot.net_pipeline_listener_on,
-        snapshot.net_pipeline_listener_off,
-        snapshot.net_pipeline_accept_wait_count,
-        snapshot.net_pipeline_accept_wait_ms_total,
-        snapshot.net_pipeline_accept_wait_ms_max,
-    );
-    let _ = uart_write_all(uart, net_pipeline_line.as_bytes()).await;
-
-    let mut net_accept_line = heapless::String::<320>::new();
-    let _ = write!(
-        &mut net_accept_line,
-        "METRICS NET_ACCEPT arm_gap_n={} arm_gap_us={} arm_gap_us_max={} arm_gap_after_mkdir_n={} arm_gap_after_mkdir_us={} arm_gap_after_mkdir_us_max={}\r\n",
-        snapshot.net_pipeline_accept_arm_gap_count,
-        snapshot.net_pipeline_accept_arm_gap_us_total,
-        snapshot.net_pipeline_accept_arm_gap_us_max,
-        snapshot.net_pipeline_accept_arm_gap_after_mkdir_count,
-        snapshot.net_pipeline_accept_arm_gap_after_mkdir_us_total,
-        snapshot.net_pipeline_accept_arm_gap_after_mkdir_us_max,
-    );
-    let _ = uart_write_all(uart, net_accept_line.as_bytes()).await;
-}
-
-pub(super) async fn write_telemetry_status_line(uart: &mut SerialUart) {
-    let mask = telemetry::diag_mask();
-    let mut line = heapless::String::<192>::new();
-    let _ = write!(
-        &mut line,
-        "TELEM mask=0x{:02x} wifi={} reassoc={} net={} http={} sd={}\r\n",
-        mask,
-        on_off(mask, telemetry::DIAG_DOMAIN_WIFI),
-        on_off(mask, telemetry::DIAG_DOMAIN_REASSOC),
-        on_off(mask, telemetry::DIAG_DOMAIN_NET),
-        on_off(mask, telemetry::DIAG_DOMAIN_HTTP),
-        on_off(mask, telemetry::DIAG_DOMAIN_SD),
-    );
-    let _ = uart_write_all(uart, line.as_bytes()).await;
-}
-
-pub(super) async fn run_telemetry_set_command(
-    uart: &mut SerialUart,
-    operation: TelemetrySetOperation,
-) {
-    let mask = match operation {
-        TelemetrySetOperation::Domain { domain, enabled } => {
-            telemetry::diag_set_domain(telemetry_domain_mask(domain), enabled)
-        }
-        TelemetrySetOperation::All { enabled } => {
-            telemetry::diag_set_mask(if enabled { telemetry::DIAG_MASK_ALL } else { 0 })
-        }
-        TelemetrySetOperation::Default => telemetry::diag_set_mask(telemetry::DIAG_MASK_DEFAULT),
-    };
-
-    let mut line = heapless::String::<192>::new();
-    let _ = write!(
-        &mut line,
-        "TELEMSET OK mask=0x{:02x} wifi={} reassoc={} net={} http={} sd={}\r\n",
-        mask,
-        on_off(mask, telemetry::DIAG_DOMAIN_WIFI),
-        on_off(mask, telemetry::DIAG_DOMAIN_REASSOC),
-        on_off(mask, telemetry::DIAG_DOMAIN_NET),
-        on_off(mask, telemetry::DIAG_DOMAIN_HTTP),
-        on_off(mask, telemetry::DIAG_DOMAIN_SD),
-    );
-    let _ = uart_write_all(uart, line.as_bytes()).await;
-}
-
-fn telemetry_domain_mask(domain: TelemetryDomain) -> u32 {
-    match domain {
-        TelemetryDomain::Wifi => telemetry::DIAG_DOMAIN_WIFI,
-        TelemetryDomain::Reassoc => telemetry::DIAG_DOMAIN_REASSOC,
-        TelemetryDomain::Net => telemetry::DIAG_DOMAIN_NET,
-        TelemetryDomain::Http => telemetry::DIAG_DOMAIN_HTTP,
-        TelemetryDomain::Sd => telemetry::DIAG_DOMAIN_SD,
-    }
-}
-
-fn on_off(mask: u32, domain: u32) -> &'static str {
-    if (mask & domain) != 0 {
-        "on"
-    } else {
-        "off"
-    }
 }
---
+++ src/firmware/runtime/serial_task/metrics/metrics_net.rs
@@ -0,0 +1,73 @@
+use core::fmt::Write;
+
+use crate::firmware::{telemetry, types::SerialUart};
+
+use super::write_line;
+
+pub(super) async fn write_metrics_net_lines(uart: &mut SerialUart) {
+    let snapshot = telemetry::snapshot();
+    let ip = snapshot.upload_http_ipv4.unwrap_or([0, 0, 0, 0]);
+
+    let mut net_line = heapless::String::<160>::new();
+    let _ = write!(
+        &mut net_line,
+        "METRICS NET wifi_connected={} http_listening={} ip={}.{}.{}.{}\r\n",
+        if snapshot.wifi_link_connected { 1 } else { 0 },
+        if snapshot.upload_http_listening { 1 } else { 0 },
+        ip[0],
+        ip[1],
+        ip[2],
+        ip[3],
+    );
+    write_line(uart, net_line).await;
+
+    let mut liveness_line = heapless::String::<224>::new();
+    let _ = write!(
+        &mut liveness_line,
+        "METRICS LIVENESS accept_link_reset={} health={} wifi_watchdog_disc={}\r\n",
+        snapshot.upload_http_accept_link_resets,
+        snapshot.upload_http_health_requests,
+        snapshot.wifi_connected_watchdog_disconnects,
+    );
+    write_line(uart, liveness_line).await;
+
+    let mut boot_line = heapless::String::<96>::new();
+    let _ = write!(
+        &mut boot_line,
+        "METRICS BOOT reset_code={}\r\n",
+        snapshot.boot_reset_reason_code,
+    );
+    write_line(uart, boot_line).await;
+
+    let mut net_pipeline_line = heapless::String::<512>::new();
+    let _ = write!(
+        &mut net_pipeline_line,
+        "METRICS NET_PIPELINE dhcp_wait_n={} dhcp_wait_ms={} dhcp_wait_ms_max={} dhcp_ready={} gate_wifi_down={} gate_link_down={} gate_no_ipv4={} listener_on={} listener_off={} accept_wait_n={} accept_wait_ms={} accept_wait_ms_max={}\r\n",
+        snapshot.net_pipeline_dhcp_wait_count,
+        snapshot.net_pipeline_dhcp_wait_ms_total,
+        snapshot.net_pipeline_dhcp_wait_ms_max,
+        snapshot.net_pipeline_dhcp_ready_count,
+        snapshot.net_pipeline_gate_wifi_down,
+        snapshot.net_pipeline_gate_link_down,
+        snapshot.net_pipeline_gate_no_ipv4,
+        snapshot.net_pipeline_listener_on,
+        snapshot.net_pipeline_listener_off,
+        snapshot.net_pipeline_accept_wait_count,
+        snapshot.net_pipeline_accept_wait_ms_total,
+        snapshot.net_pipeline_accept_wait_ms_max,
+    );
+    write_line(uart, net_pipeline_line).await;
+
+    let mut net_accept_line = heapless::String::<320>::new();
+    let _ = write!(
+        &mut net_accept_line,
+        "METRICS NET_ACCEPT arm_gap_n={} arm_gap_us={} arm_gap_us_max={} arm_gap_after_mkdir_n={} arm_gap_after_mkdir_us={} arm_gap_after_mkdir_us_max={}\r\n",
+        snapshot.net_pipeline_accept_arm_gap_count,
+        snapshot.net_pipeline_accept_arm_gap_us_total,
+        snapshot.net_pipeline_accept_arm_gap_us_max,
+        snapshot.net_pipeline_accept_arm_gap_after_mkdir_count,
+        snapshot.net_pipeline_accept_arm_gap_after_mkdir_us_total,
+        snapshot.net_pipeline_accept_arm_gap_after_mkdir_us_max,
+    );
+    write_line(uart, net_accept_line).await;
+}
